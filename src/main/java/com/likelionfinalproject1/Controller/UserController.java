c8b8967be2f479324e76526d04cc7b3b7947fbba
Bae Ji Won <qowhxk@naver.com>
2022-12-22 11:13:54 +0900

    #12/22 🚀 [feat] User Controller 로그인 기능 추가

--- src/main/java/com/likelionfinalproject1/Controller/UserController.java
+++ src/main/java/com/likelionfinalproject1/Controller/UserController.java
@@ -1,9 +1,7 @@
 package com.likelionfinalproject1.Controller;
 
 import com.likelionfinalproject1.Domain.Response;
-import com.likelionfinalproject1.Domain.dto.UserJoinDao;
-import com.likelionfinalproject1.Domain.dto.UserJoinRequest;
-import com.likelionfinalproject1.Domain.dto.UserJoinResponse;
+import com.likelionfinalproject1.Domain.dto.*;
 import com.likelionfinalproject1.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +18,12 @@ public class UserController {
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userRequest){
         UserJoinDao userJoinDao = userService.join(userRequest);
         return Response.success(new UserJoinResponse(userJoinDao.getId(),userJoinDao.getUsername()));
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest userLoginRequest){
+        String token = userService.login(userLoginRequest);
+        return Response.success(new UserLoginResponse(token));
     }
 }
