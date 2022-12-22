43a5d2f72173c75d615ec58b6ea597566fd40c0c
Bae Ji Won <qowhxk@naver.com>
2022-12-22 11:15:14 +0900

    #12/22 🚀 [feat] UserLoginRequest DTO 생성

---
+++ src/main/java/com/likelionfinalproject1/Domain/dto/UserLoginRequest.java
@@ -0,0 +1,12 @@
+package com.likelionfinalproject1.Domain.dto;
+
+import com.likelionfinalproject1.Domain.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserLoginRequest {
+    private String username;
+    private String password;
+}
