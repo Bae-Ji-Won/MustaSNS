ba80d6b965de0e67b8c1dc70197c53b3ab2c9b30
Bae Ji Won <qowhxk@naver.com>
2022-12-22 11:13:30 +0900

    #12/22 🚀 [feat] Jwt Token 설정 파일 추가

---
+++ src/main/java/com/likelionfinalproject1/utils/JwtTokenUtil.java
@@ -0,0 +1,23 @@
+package com.likelionfinalproject1.utils;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtTokenUtil {
+    public static String createToken(String userName, long expireTimeMs, String key) {
+        Claims claims = Jwts.claims(); // 일종의 map
+        claims.put("userName", userName);
+
+        return Jwts.builder()       // 토큰 생성
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))      //  시작 시간 : 현재 시간기준으로 만들어짐
+                .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs))     // 끝나는 시간 : 지금 시간 + 정해둔 시간
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact()
+                ;
+    }
+}
