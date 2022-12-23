b7387e3f3c1e510ff8b69302745c34f06c99c187
Bae Ji Won <qowhxk@naver.com>
2022-12-23 09:46:13 +0900

    #12/23 🚀 [feat] swagger

---
+++ src/main/java/com/likelionfinalproject1/Controller/SwaggerTestController2.java
@@ -0,0 +1,15 @@
+package com.likelionfinalproject1.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class SwaggerTestController2 {
+
+    @GetMapping("/hello")
+    public String swaggerTest(){
+        return "popin";
+    }
+}
