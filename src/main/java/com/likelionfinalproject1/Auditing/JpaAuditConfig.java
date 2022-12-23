b72adc52abafd1c30ee4631a0f71d445de6a312d
Bae Ji Won <qowhxk@naver.com>
2022-12-23 09:12:51 +0900

    #12/23 📝 [feat] Audit add(날짜 설정 파일)

--- src/main/java/com/likelionfinalproject1/Auditing/JpaAuditConfig.java
+++ src/main/java/com/likelionfinalproject1/Auditing/JpaAuditConfig.java
@@ -3,6 +3,7 @@ package com.likelionfinalproject1.Auditing;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// jpa에서 실시간 시간측정하기 하기 위한 클래스(데이터 생성, 수정, 삭제)
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditConfig {
