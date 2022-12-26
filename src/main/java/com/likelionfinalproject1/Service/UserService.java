19fdbfe909e06ca7cac13ee9a61de6857bcb43d5
Bae Ji Won <qowhxk@naver.com>
2022-12-26 09:09:33 +0900

    #12/25 😎 [refactor] UserEntity -> User (Entity 이름 변경)

--- src/main/java/com/likelionfinalproject1/Service/UserService.java
+++ src/main/java/com/likelionfinalproject1/Service/UserService.java
@@ -1,9 +1,9 @@
 package com.likelionfinalproject1.Service;
 
-import com.likelionfinalproject1.Domain.UserEntity;
-import com.likelionfinalproject1.Domain.dto.UserJoinDao;
-import com.likelionfinalproject1.Domain.dto.UserJoinRequest;
-import com.likelionfinalproject1.Domain.dto.UserLoginRequest;
+import com.likelionfinalproject1.Domain.Entity.User;
+import com.likelionfinalproject1.Domain.dto.User.UserJoinDao;
+import com.likelionfinalproject1.Domain.dto.User.UserJoinRequest;
+import com.likelionfinalproject1.Domain.dto.User.UserLoginRequest;
 import com.likelionfinalproject1.Exception.AppException;
 import com.likelionfinalproject1.Exception.ErrorCode;
 import com.likelionfinalproject1.Repository.UserRepository;
@@ -33,7 +33,7 @@ public class UserService {
                     throw new AppException(ErrorCode.DUPLICATED_USER_NAME,String.format(userJoinRequest.getUserName())+"는 이미 있습니다.");
                 });
 
-        UserEntity userEntity = userRepository.save(userJoinRequest.toEntity(encoder.encode(userJoinRequest.getPassword())));
+        User userEntity = userRepository.save(userJoinRequest.toEntity(encoder.encode(userJoinRequest.getPassword())));
 
         return UserJoinDao.fromEntity(userEntity);
     }
@@ -44,22 +44,27 @@ public class UserService {
         String password = userLoginRequest.getPassword();
 
         log.info("사용자가 입력한 username : "+username);
-        UserEntity user = userRepository.findByUserName(username)     // 유저가 입력한 데이터의 아이디를 가지고 DB에서 데이터를 찾음
+        User user = userRepository.findByUserName(username)     // 유저가 입력한 데이터의 아이디를 가지고 DB에서 데이터를 찾음
                 // 아이디가 존재하지 않을때
-                .orElseThrow(()->new AppException(ErrorCode.USERNAME_NOTFOUND,null));
+                .orElseThrow(()->new AppException(ErrorCode.USERNAME_NOTFOUND));
 
 
         // 아이디가 존재하지만 비밀번호가 틀릴때
         if(!encoder.matches(password,user.getPassword())){
-            throw new AppException(ErrorCode.INVALID_PASSWORD,null);
+            throw new AppException(ErrorCode.INVALID_PASSWORD);
         }
 
         // 아이디가 존재하고 비밀번호도 일치할때
         return JwtTokenUtil.createToken(username,expireTimeMs,secretkey);
     }
 
-    public UserEntity getUserByUserName(String userName) {
-        return userRepository.findByUserName(userName)
-                .orElseThrow(() -> new AppException(ErrorCode.INVALID_PERMISSION,null));
+
+    // UserName을 통해 DB에 데이터가 있는지 확인(유저가 존재하는지 확인)
+    public User getUserByUserName(String userName) {
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new AppException(ErrorCode.INVALID_PERMISSION,String.format("해당 유저가 없습니다.")));
+        return user;
     }
+
+
 }
