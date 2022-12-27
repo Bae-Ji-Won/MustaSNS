24ef497f9e9ae17666ff5ff8cb8b639cbeaaefaa
Bae Ji Won <qowhxk@naver.com>
2022-12-27 09:04:53 +0900

    #12/25 🚀 [feat] post 게시물 update 기능 추가

--- src/main/java/com/likelionfinalproject1/Service/PostService.java
+++ src/main/java/com/likelionfinalproject1/Service/PostService.java
@@ -2,14 +2,13 @@ package com.likelionfinalproject1.Service;
 
 import com.likelionfinalproject1.Domain.Entity.Post;
 import com.likelionfinalproject1.Domain.Entity.User;
-import com.likelionfinalproject1.Domain.dto.Post.PostCreateRequest;
-import com.likelionfinalproject1.Domain.dto.Post.PostCreateResponse;
-import com.likelionfinalproject1.Domain.dto.Post.PostListResponse;
-import com.likelionfinalproject1.Domain.dto.Post.PostOneResponse;
+import com.likelionfinalproject1.Domain.dto.Post.*;
 import com.likelionfinalproject1.Exception.AppException;
 import com.likelionfinalproject1.Exception.ErrorCode;
 import com.likelionfinalproject1.Repository.PostRepository;
+import com.likelionfinalproject1.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -18,9 +17,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
     private final PostRepository postRepository;
     private final UserService userService;
+    private final UserRepository userRepository;
 
     // 포스트 새로 작성
     public PostCreateResponse postcreate(PostCreateRequest postCreateRequest, String userName){
@@ -51,4 +52,27 @@ public class PostService {
         return postListResponses;
     }
 
+    // 포스트 게시물 수정
+    public PostUpdateResponse postupdate(Long id, PostCreateRequest request, String userName) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.DATABASE_ERROR));     // DB에 해당 id의 데이터가 없을경우 에외처리
+
+        User user = userRepository.findByUserName(userName)                 // 포스트는 있으나 유저가 없을경우
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND));
+
+        if(!userName.equals(post.getUserId().getUserName())){     // 현재 토큰에 있는 아이디와 게시물의 아이디가 다를경우 예외처리
+            throw new AppException(ErrorCode.INVALID_PERMISSION);
+        }
+
+        if(!request.getTitle().equals(post.getTitle())){     // 서버에 저장되어 있는 데이터와 유저가 새로 입력한 데이터가 다를경우 덮어씌움
+            post.setTitle(request.getTitle());
+        }
+        if(!request.getBody().equals(post.getBody())){
+            post.setBody(request.getBody());
+        }
+
+        postRepository.save(post);
+
+        return PostUpdateResponse.success(post.getId());    // 정상동작시 "등록 완료" 출력
+    }
 }
