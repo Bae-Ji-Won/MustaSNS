913e7919a25ac5fe3721b575dceec1e9681243fe
Bae Ji Won <qowhxk@naver.com>
2022-12-27 09:06:32 +0900

    #12/25 🚀 [feat] builder 추가

--- src/main/java/com/likelionfinalproject1/Domain/dto/User/UserLoginRequest.java
+++ src/main/java/com/likelionfinalproject1/Domain/dto/User/UserLoginRequest.java
@@ -1,10 +1,12 @@
 package com.likelionfinalproject1.Domain.dto.User;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class UserLoginRequest {
     private String userName;
     private String password;
