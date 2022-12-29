357550243e738d27b037ed71e680be612ff46e81
Bae Ji Won <qowhxk@naver.com>
2022-12-29 09:54:21 +0900

    #12/29 🚀 [feat] 알고리즘 문제 추가

--- src/main/java/com/likelionfinalproject1/Controller/SwaggerTestController.java
+++ src/main/java/com/likelionfinalproject1/Controller/SwaggerTestController.java
@@ -1,16 +1,26 @@
 package com.likelionfinalproject1.Controller;
 
 
+import com.likelionfinalproject1.Service.AlgorithmService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class SwaggerTestController {
+
+    private final AlgorithmService algorithmService;
 
     @GetMapping("/hello")
     public String swaggerTest(){
         return "배지원";
+    }
+
+    @GetMapping("/{num}")
+    public Integer divide(Integer num){
+        return algorithmService.sumOfDigit(num);
     }
 }
