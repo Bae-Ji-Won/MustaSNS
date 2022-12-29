9a2f0d80bc44f23396d080af535fc7049269524c
Bae Ji Won <qowhxk@naver.com>
2022-12-29 10:27:40 +0900

    #12/29 🚀 [feat] api 주소 변경

--- src/main/java/com/likelionfinalproject1/Controller/SwaggerTestController.java
+++ src/main/java/com/likelionfinalproject1/Controller/SwaggerTestController.java
@@ -3,24 +3,35 @@ package com.likelionfinalproject1.Controller;
 
 import com.likelionfinalproject1.Service.AlgorithmService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/hello")
 @RequiredArgsConstructor
+@Slf4j
 public class SwaggerTestController {
 
     private final AlgorithmService algorithmService;
 
-    @GetMapping("/hello")
+    @GetMapping
     public String swaggerTest(){
         return "배지원";
     }
 
     @GetMapping("/{num}")
-    public Integer divide(Integer num){
+    public Integer divide(@PathVariable(name = "num") int num){
+        log.info("num:"+num);
         return algorithmService.sumOfDigit(num);
     }
+
+
+//    @GetMapping("/{num}")
+//    public Integer divide(@PathVariable(name = "num") Integer num){
+//        log.info("num:"+num);
+//        return algorithmService.sumOfDigit(num);
+//    }
 }
