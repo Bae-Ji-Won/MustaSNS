bc1108c680c6b27dc1c0c6e2dc4173d0232acb01
Bae Ji Won <qowhxk@naver.com>
2023-01-02 22:53:42 +0900

    #01/02 🚀 [feat] 유저 정보 출력, 유저 권한 변경 기능 추가

--- src/main/java/com/likelionfinalproject1/Controller/UserController.java
+++ src/main/java/com/likelionfinalproject1/Controller/UserController.java
@@ -1,9 +1,14 @@
 package com.likelionfinalproject1.Controller;
 
+import com.likelionfinalproject1.Domain.Entity.User;
 import com.likelionfinalproject1.Domain.Response;
+import com.likelionfinalproject1.Domain.UserRole;
 import com.likelionfinalproject1.Domain.dto.User.*;
 import com.likelionfinalproject1.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,5 +30,27 @@ public class UserController {
     public Response<UserLoginResponse> login(@RequestBody UserLoginRequest userLoginRequest){
         String token = userService.login(userLoginRequest);
         return Response.success(new UserLoginResponse(token));
+    }
+
+    // 모든 유저 데이터 출력
+    @GetMapping
+    public Response<Page<User>> userFindAll(Pageable pageable){
+        Page<User> userlist = userService.findAllByPage(pageable);
+        return Response.success(userlist);
+    }
+
+    // 해당 유저 데이터 출력
+    @GetMapping("/{id}")
+    public Response<User> userFind(@PathVariable(name = "id") Long id){
+        User user = userService.findById(id);
+        return Response.success(user);
+    }
+
+    // 관리자 권한 부여 기능 userid를 보냄
+    @PostMapping("/{id}/role/change")
+    public Response<String> userToAdmin(@PathVariable(name = "id")Long userid, Authentication authentication){
+        String name = authentication.getName();
+        userService.userToAdmin(userid,name);
+        return Response.success(String.format(name+"에 해당하는 유저의 권한이 변경됬습니다."));
     }
 }
