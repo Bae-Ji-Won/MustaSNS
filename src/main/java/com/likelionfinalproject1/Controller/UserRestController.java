e13ffd79b5a6cec67701c513a6b48ead8965a0dd
Bae Ji Won <qowhxk@naver.com>
2023-01-04 03:04:49 +0900

    #01/03 😎 [refactor] Dao -> Dto 파일명 변경

--- src/main/java/com/likelionfinalproject1/Controller/UserRestController.java
+++ src/main/java/com/likelionfinalproject1/Controller/UserRestController.java
@@ -2,7 +2,6 @@ package com.likelionfinalproject1.Controller;
 
 import com.likelionfinalproject1.Domain.Entity.User;
 import com.likelionfinalproject1.Domain.Response;
-import com.likelionfinalproject1.Domain.UserRole;
 import com.likelionfinalproject1.Domain.dto.User.*;
 import com.likelionfinalproject1.Service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +13,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
-public class UserController {
+public class UserRestController {
 
     private final UserService userService;
 
     // 회원 가입
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userRequest){
-        UserJoinDao userJoinDao = userService.join(userRequest);
-        return Response.success(new UserJoinResponse(userJoinDao.getId(),userJoinDao.getUsername()));
+        UserJoinDto userJoinDto = userService.join(userRequest);
+        return Response.success(new UserJoinResponse(userJoinDto.getId(),userJoinDto.getUsername()));
     }
 
     // 로그인
