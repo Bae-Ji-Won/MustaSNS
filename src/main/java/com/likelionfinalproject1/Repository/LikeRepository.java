3a1d8ba2bc1a6e47449d2a0ded54c62bd6109e9e
Bae Ji Won <qowhxk@naver.com>
2023-01-06 00:45:49 +0900

    #01/04 🚀 [feat] Post 삭제시 좋아요 삭제 기능 추가

--- src/main/java/com/likelionfinalproject1/Repository/LikeRepository.java
+++ src/main/java/com/likelionfinalproject1/Repository/LikeRepository.java
@@ -1,12 +1,21 @@
 package com.likelionfinalproject1.Repository;
 
 import com.likelionfinalproject1.Domain.Entity.Like;
+import com.likelionfinalproject1.Domain.Entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like,Long> {
     Optional<Like> findByPostIdAndUserId(Long postId, Long userId);
+
+    @Transactional
+    @Modifying
+    // Post(Entity)에 해당하는 데이터는 모두 삭제
+    void deleteAllByPost(@Param("post") Post post);     // @Param("post") = post라는 이름으로 Post(Entity)가 넘어온다는 뜻
 }
