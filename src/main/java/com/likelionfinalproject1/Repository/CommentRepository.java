a08de33b027a3dcda7b89b3344c6fb5620715b72
Bae Ji Won <qowhxk@naver.com>
2023-01-08 00:02:38 +0900

    #01/08 🚀 [feat] Comment Repoistory 기능 추가(포스트에 대한 모든 데이터 찾기)

--- src/main/java/com/likelionfinalproject1/Repository/CommentRepository.java
+++ src/main/java/com/likelionfinalproject1/Repository/CommentRepository.java
@@ -1,14 +1,25 @@
 package com.likelionfinalproject1.Repository;
 
 import com.likelionfinalproject1.Domain.Entity.Comment;
+import com.likelionfinalproject1.Domain.Entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     Page<Comment> findByPostId(Long id,Pageable pageable);      
     // findByPostId는 Comment Entity에 저장되어 있는 post의 부분과 일치해야함
     // 즉, Comment의 post변수의 Id의 값을 통해 구한다는 의미로 post().Id와 같음
+
+
+    @Transactional
+    @Modifying
+        // Post(Entity)에 해당하는 데이터는 모두 삭제
+    void deleteAllByPost(@Param("post") Post post);     // @Param("post") = post라는 이름으로 Post(Entity)가 넘어온다는 뜻
 }
