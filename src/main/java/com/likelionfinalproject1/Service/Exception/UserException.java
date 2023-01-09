9a902834cbe0140fd8d34ccf53ea7b34bd89f0fe
Bae Ji Won <qowhxk@naver.com>
2023-01-09 16:01:44 +0900

    #01/09 🚀 [feat] UserException

--- src/main/java/com/likelionfinalproject1/Service/Exception/UserException.java
+++ src/main/java/com/likelionfinalproject1/Service/Exception/UserException.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Component
 public class UserException {
@@ -20,10 +22,16 @@ public class UserException {
                 .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND));
     }
 
-    // UserName을 통해 DB에 데이터가 있는지 확인(유저가 존재하는지 확인)
+    // UserName을 통해 DB에 데이터가 있는지 확인(유저가 존재하는지 확인)  - jwt토큰 설정에서만 사용
     public User getUserByUserName(String userName) {
         User user = userRepository.findByUserName(userName)
                 .orElseThrow(() -> new AppException(ErrorCode.INVALID_PERMISSION,String.format("해당 유저가 없습니다.")));
+        return user;
+    }
+    
+    // PostService - postcreate 에서만 사용
+    public Optional<User> testGetUserByUserName(String userName){
+        Optional<User> user = userRepository.findByUserName(userName);
         return user;
     }
 }
